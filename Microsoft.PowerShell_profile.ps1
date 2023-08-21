4f38fd27a9379ef05cb5ea81d8ecb83dd1e1d650
Joshua Donovan <Josh@alcanweb.com>
2023-08-20 20:29:40 -0500

    v1.0

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,106 +4,154 @@ $PowerShell.runspace = $Runspace
 $Runspace.Open()
 
 [void]$PowerShell.AddScript({
-    
-    # SETUP Environment for Queries
-    # This example uses the PowerShell SecretStore to store / retrieve the Azure App Registration informaiton so it is not stored in plain text.
-    # https://github.com/powershell/secretmanagement
-
+    $ErrorActionPreference = "Stop"
     If (!(Get-Module -Name Microsoft.PowerShell.SecretStore)) {
         try {
             Import-Module -Name Microsoft.PowerShell.SecretStore
-            $env:POSH_AZURE_STATUS = "Successfully Imported Microsoft.PowerShell.SecretStore"
         } Catch {
-            $env:POSH_AZURE_STATUS = "Could not `"Import-Module -Name Microsoft.PowerShell.SecretStore`" error: $($error[0] | select *)"
+            $env:POSH_AZURE_STATUS = "Could not `"Import-Module -Name Microsoft.PowerShell.SecretStore`" error: $($error[0] | Select-Object *)"
             return
         }
     }
     try {
         $TenantId = Get-Secret -Name POSH_AZURE_TENANT_ID -Vault PWSH_PROFILE -AsPlainText
+        $SubscriptionId = Get-Secret -Name POSH_AZURE_SUBSCRIPTION_ID -Vault PWSH_PROFILE -AsPlainText
         $client_id = Get-Secret -Name POSH_AZURE_CLIENT_ID -Vault PWSH_PROFILE -AsPlainText
         $client_secret = Get-Secret -Name POSH_AZURE_CLIENT_SECRET -Vault PWSH_PROFILE
         $cred = [PsCredential]::New($client_id, $client_secret)
-        $env:POSH_AZURE_STATUS = "Service Principal Credentials Configured"
+        $env:POSH_AZURE_STATUS = "Starting"
     } catch {
-        $env:POSH_AZURE_STATUS = "Something went wrong configuring Service Principal Credentials error: $($error[0] | select *)"
+        $env:POSH_AZURE_STATUS = "Something went wrong configuring Service Principal Credentials error: $($error[0] | Select-Object *)"
+        return
     }
-    try {
-        Connect-AzAccount -ServicePrincipal -Credential $cred -Tenant $TenantId -Context POSH_QUERIES
-        $env:POSH_AZURE_STATUS = "Connect-AzAccount Completed"
-    } catch {
-        $env:POSH_AZURE_STATUS = "Connect-AzAccount Failed error: $($error[0] | select *)"
-    }
-    function Login()  
-    {  
-        $context = Get-AzContext -Name POSH_QUERIES
-        if (!$context) 
-        {  
-            try {
-                Connect-AzAccount -ServicePrincipal -Credential $cred -Tenant $TenantId -Context POSH_QUERIES
-                return $true
-            } catch {
-                return $false
-            }
-        } else {
-            return $true
-        }
-        if ((Get-AzContext).Name -ne "POSH_QUERIES")
-        {
-            Set-AzContext -Context $(Get-AzContext -Name POSH_QUERIES)
-        }
-    }  
     function Get-VMStates()
     {
         try {
-            $GraphData = Search-AzGraph -Query 'resources | where type == "microsoft.compute/virtualmachines" | extend powerState = properties.extended.instanceView.powerState.displayStatus | where powerState != "VM deallocated" | project name'
+            $jobs = Get-Job | Where-Object {$_.Name -eq "GETVMSTATES"}
+            if ($jobs) {
+                $jobs | Remove-Job -Force
+            }
+            $jobValue = Start-Job -Name GETVMSTATES -ArgumentList @($cred, $SubscriptionId, $TenantId) -ScriptBlock {
+                $null = Connect-AzAccount -ContextName POSH_QUERIES_JOB -ServicePrincipal -Credential $args[0] -SubscriptionId $args[1] -Tenant $args[2] -SkipContextPopulation -Force
+                $GraphData = Search-AzGraph -Query 'resources | where type == "microsoft.compute/virtualmachines" | extend powerState = properties.extended.instanceView.powerState.displayStatus | where powerState != "VM deallocated" | project name'
+                Remove-AzContext -Name POSH_QUERIES_JOB -Force
+                if ($GraphData.Count -ge 1) {
+                    $formattedString = "·"
+                    $($GraphData | Foreach-Object {$formattedString = "$formattedString 󰒋 $($_.name) 󰶼 ·"})
+                    return $formattedString
+                } else {
+                    return
+                }
+            } | Wait-Job | Receive-Job
         } catch {
-            return "ARG Request Failed"
+            return "ARG Request Failed error: $($error[0] | Select-Object *)"
         }
-
-        if ($GraphData.Count -ge 1) {
-            $formattedString = "·"
-            $($GraphData | Foreach-Object {$formattedString = "$formattedString 󰒋 $($_.name) 󰶼 ·"})
-            return $formattedString
+        $jobs = Get-Job | Where-Object {$_.Name -eq "GETVMSTATES"}
+        if ($jobs) {
+            $jobs | Remove-Job -Force
+        }
+        if($jobValue) {
+            return $jobValue
         } else {
             return
         }
     }
-
+    function Get-AzureCostReport
+    {
+        try {
+            $jobs = Get-Job | Where-Object {$_.Name -eq "GETAZURECOSTREPORT"}
+            if ($jobs) {
+                $jobs | Remove-Job -Force
+            }
+            $jobValue = Start-Job -Name GETAZURECOSTREPORT -ArgumentList @($cred, $SubscriptionId, $TenantId) -ScriptBlock {
+                $null = Connect-AzAccount -ContextName POSH_QUERIES_BILLING_JOB -ServicePrincipal -Credential $args[0] -SubscriptionId $args[1] -Tenant $args[2] -SkipContextPopulation -Force
+                $Data = Get-AzConsumptionUsageDetail
+                Remove-AzContext -Name POSH_QUERIES_BILLING_JOB -Force
+                if ($Data.Count -ge 1) {
+                    return $Data
+                } else {
+                    return
+                }
+            } | Wait-Job | Receive-Job
+        } catch {
+            return "Get-AzConsumptionUsageDetail: $($error[0] | Select-Object *)"
+        }
+        $jobs = Get-Job | Where-Object {$_.Name -eq "GETAZURECOSTREPORT"}
+        if ($jobs) {
+            $jobs | Remove-Job -Force
+        }
+        if($jobValue) {
+            return $jobValue
+        } else {
+            return
+        } 
+    }
     function Get-DaysTillReset()
     {
         $today = Get-Date
         $reset = (Get-Date (Get-Date $today -Day 1 ).AddMonths(1) -Day 15)
         return "$(($reset - $today).Days)"
     }
-
     function Get-AzureCost($Report)
-    {	
-        $cost = ($Report | Measure-Object PretaxCost -Sum).Sum.ToString("##0.00")
+    {	try {
+            $cost = ($Report | Measure-Object PretaxCost -Sum).Sum.ToString("##0.00")
+        } catch {
+            $env:POSH_AZURE_STATUS = "Get-AzureCost Failed: $($error[0] | Select-Object *)"
+            return "$($TagValue): Error Finding Tags"
+        }
         return $cost
     }
     function Get-AzureCostbyTag($Report, $TagName, $TagValue)
     {
-        $taggedResources = $Report | Where-Object {$_.Tags -ne $null}
+        $taggedResources = $Report | Where-Object {$null -ne $_.Tags}
         if ($taggedResources)
         {
-            $CostbyTagValue = ($taggedResources | Where-Object {$_.Tags[$TagName] -eq $TagValue} | Measure-Object PretaxCost -Sum).Sum.ToString("##0.000")
-            return "$($TagValue): `$$CostbyTagValue"
-        }
-        else {
-            return "No Matching Tags"
+            try {
+                $FilteredResources = $taggedResources | Where-Object {$_.Tags[$TagName] -eq $TagValue}
+            } catch {
+                $env:POSH_AZURE_STATUS = "FilteredResources error: $($error[0] | Select-Object *)"
+                return "$($TagValue): Error Finding Tags"
+            }
+            if ($FilteredResources.Count -gt 0) {
+                try {
+                    $CostbyTagValue = ($FilteredResources | Measure-Object PretaxCost -Sum).Sum.ToString("##0.000")
+                } catch {
+                    $env:POSH_AZURE_STATUS = "CostbyTagValue error: $($error[0] | Select-Object *)"
+                    return "$($TagValue): Cost Calculation Error"
+                }
+            } else {
+                return "$($TagValue): No Matched Resources"
+            }
+            if ($CostbyTagValue) {
+                return "$($TagValue): `$$CostbyTagValue"
+            } else {
+                return "Issue Calculating Cost"
+            }
+        } else {
+            return "No Tagged Resources"
         }
     }
-    $running = $true
+    $firstRun = $true
     # do work
     do {
-
-        if (Login) {
-            $env:POSH_AZURE_STATUS = "Loop Started"
-            $CostReport = Get-AzConsumptionUsageDetail
+            if ($firstRun) {
+                $env:POSH_AZURE_STATUS = "Loop Started"
+            }
+            Try {
+                $CostReport = Get-AzureCostReport
+                if ($firstRun) {
+                    $env:POSH_AZURE_STATUS = "Got Consumption Details"
+                }
+            } Catch {
+                $env:POSH_AZURE_STATUS = "Error getting Consumption Details error: $($error[0] | Select-Object *)"
+            }
             $env:POSH_AZURE_TOTAL_COST = Get-AzureCost -Report $CostReport
             if($env:POSH_AZURE_TOTAL_COST) {
                 # My chosen budget is $150.00
                 $env:POSH_AZURE_TOTAL_REMAINING = $(150.00 - $env:POSH_AZURE_TOTAL_COST)
+            }
+            if ($CostReport){
+                $env:POSH_MONITORED_AZURE_SUBSCRIPTION = "$($CostReport[0].SubscriptionName)"
             }
             $env:POSH_AZURE_TIME_TILL_RESET = Get-DaysTillReset
             $env:POSH_AZURE_COST_SERVERS = Get-AzureCostbyTag -Report $CostReport -TagName 'app_group' -TagValue 'servers'
@@ -111,14 +159,21 @@ $Runspace.Open()
             $env:POSH_AZURE_RUNNING_SERVERS = Get-VMStates
             # Hide Status Segment after loop is running without error
             $env:POSH_AZURE_STATUS = ""
+            $firstRun = $false
             start-sleep 300
-        } else {
-            $running = $false
-        }
-    } while ($running)
+    } while ($true)
+    $env:POSH_AZURE_STATUS = "Exited Query Loop"
 })
 
 $AsyncObject = $PowerShell.BeginInvoke()
 
-oh-my-posh init pwsh --config "$ENV:USERPROFILE\Documents\WindowsPowerShell\cheap-cloud-azure.omp.json" | Invoke-Expression
+# Checks for correct userprofile path
+if (Test-Path "$ENV:OneDrive\Documents\WindowsPowerShell\cheap-cloud-azure.omp.json")
+{
+    $configPath = "$ENV:OneDrive\Documents\WindowsPowerShell\cheap-cloud-azure.omp.json"
+} else {
+    $configPath = "$ENV:USERPROFILE\Documents\WindowsPowerShell\cheap-cloud-azure.omp.json"
+}
+
+oh-my-posh init pwsh --config $configPath | Invoke-Expression
 $env:POSH_AZURE_ENABLED = $true
--- README.md
+++ README.md
@@ -1,2 +1,170 @@
-# Oh_My_POSH
-A repository for my OH_MY_POSH configuration.
+# Oh_My_POSH 
+> Everything below is just for fun, this is not intended to be a tutorial on Az Modules, Oh My Posh...etc
+> 
+> Nothing here is guaranteed to work outside of my environment.
+>
+> Use at your own risk. This repository carry's no warranty or support.
+> 
+
+
+
+## What it does
+![Screenshot of the POSH](images\Screenshot.png)
+
+This PowerShell Profile Script and Oh My Posh Template tracks your Azure Spend in real time and even has the ability to display your desired items by tag!
+
+The PowerShell Profile Script uses runspaces to allow for async processing of your Azure Reports without slowing down your access to the console (much). Because runspaces are a new thread inside the same process and not a new process (like Start-Job) we can pass information back to Oh My Posh through environment variables. 
+
+In the current configuration, this data updates once every 5 minutes. 
+
+I did find that using the Az Module in the runspaces with a service principal was causing my main console to have its AzContext set to the service principal when the report is ran. So, that is why the Az cmdlets are run within jobs on a different process to avoid screwing with the current AzContext I might be actively using. 
+
+## Environment
+- Windows 10/11
+- Administrator Access required for installations
+- Windows Terminal (Preview) [There is a fix for icons being small available in preview]
+- PowerShell 7.3.6
+- Font/Icons: Meslo LGM NF
+
+## Dependancies
+- Oh My Posh
+- Microsoft.PowerShell.SecretManagement
+- Microsoft.PowerShell.SecretStore
+- PowerShell Az Module
+- PowerShell Az.ResourceGraph Module
+
+# Installations
+## Oh My Posh 
+[ohmyposh.dev](https://ohmyposh.dev/docs/installation/windows)
+
+Follow your preferred installation method, I personally installed from the Microsoft Store.
+
+### Configuration
+
+- Save the cheap-cloud-azure.omp.json file to your PowerShell directory. `\Documents\WindowsPowerShell\cheap-cloud-azure.omp.json`
+
+[ohmyposh Fonts](https://ohmyposh.dev/docs/installation/fonts)
+- Download your preferred Font Pack and Install in Windows Settings > Personalization > Fonts
+
+[ohmyposh docs](https://ohmyposh.dev/docs/configuration/general)
+- Good Reading if you want to understand how to customize Oh My Posh
+
+
+
+## PowerShell 7
+In a PowerShell Prompt: 
+
+`winget install --id Microsoft.Powershell --source winget`
+
+[PowerShell 7 Docs](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.3)
+
+## Windows Terminal (Preview)
+- Available for install in the Windows Store.
+- After it is installed, click the dropdown next to the +, select Settings.
+- Change Default Profile to PowerShell 7
+- Select your preferred color settings (I use One Half Dark)
+- On the "Rendering" Tab, make sure "Use the new text renderer("AtlasEngine")" is On/Enabled.
+- Towards the bottom you should see the PowerShell 7 Profile Configuration
+    - Change the Font face to the "Nerd Font" you chose to install. (I am using MesloLGL Nerd Font)
+- Save
+
+## PowerShell Modules
+### Az
+```
+Install-Module -Name Az -Repository PSGallery
+```
+### Az.ResourceGraph (Not included in default Az Module install)
+```
+Install-Module -Name Az.ResourceGraph -Repository PSGallery
+```
+### Secrets
+```
+Install-Module -Name Microsoft.PowerShell.SecretManagement -Repository PSGallery
+Install-Module -Name Microsoft.PowerShell.SecretStore -Repository PSGallery
+```
+
+## Setup your Azure Access (New Application Registration)
+[App Registration Docs](https://learn.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app)
+- Login to portal.azure.com
+- Navigate to Azure Active Directory
+- On the left find App Registrations
+- Click New Registration
+    - Give your registration a name that makes sense to you (We will be using it to periodically query Billing and Azure Resource Graph Data.)
+    - Select Single Tenant
+    - No Redirect URL is needed
+    - Register
+- On the page for your registration
+    - Copy the Application (client) ID
+    - Copy the Directory (tenant) ID
+    - Go to Certificates & Secrets
+    - Generate a new secret (copy the secret Value on this page, it will not be retrievable after you leave)
+        - Make sure you grab the secret Value and not the Secret ID (We don't need the ID)
+- Exit Azure Active Directory and go back to the main portal.azure.com
+    - Navigate to the Subscription you are wanting to grant this app permissions to.
+    - Go to Access Control (IAM)
+    - Click Add / Add role assignment
+    - I'm using "Reader", but feel free to customize what RBAC role you prefer. (after selecting the role, got to the Members tab)
+    - select User, Group or Service Principal and click + Select members
+    - Your App registration will NOT show by default, search for it by name and it should show for you to select.
+    - Assign the permissions.
+
+## Setup your secrets vault
+[Secret Manager and Secret Store Updates](https://devblogs.microsoft.com/powershell/secretmanagement-and-secretstore-updates/)
+
+[Secret Manager Github](https://github.com/powershell/secretmanagement)
+
+Provision your vault for this purpose:
+```
+Register-SecretVault -Name PWSH_PROFILE -ModuleName Microsoft.PowerShell.SecretStore
+```
+>&nbsp;
+> 
+>WARNING:
+>
+>Setting the Authentication to `None` is less secure than Password. Specifying `None` may be useful for testing scenarios but should not be used with important secrets. 
+>
+>[Cmdlet Docs](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.secretstore/set-secretstoreconfiguration?view=ps-modules#parameters)
+>
+>&nbsp;
+
+I am choosing to set my authentication to none for ease of use, don't follow this blindly and understand what this means before configuring your environment to be the same. Configure at your own risk. `If you choose not to follow this configuration you will have to customize this to use a password to unlock the vault`. 
+```
+Set-SecretStoreConfiguration -Authentication none
+```
+Fill in your application registration data we copied earlier
+```
+Set-Secret -Name POSH_AZURE_TENANT_ID -Secret "{ Tenant ID }" -Vault PWSH_PROFILE
+Set-Secret -Name POSH_AZURE_SUBSCRIPTION_ID -Secret "{ Subscription ID }" -Vault PWSH_PROFILE
+Set-Secret -Name POSH_AZURE_CLIENT_ID -Secret "{ Client ID }"
+Set-Secret -Name POSH_AZURE_CLIENT_SECRET -Secret $(ConvertTo-SecureString "{ Client Secret }" -AsPlainText) -Vault PWSH_PROFILE
+```
+
+## PowerShell Profile
+The PowerShell Profile is where most of the magic happens for my Posh Segments, because of the lag associated with running the commands to update each time a new prompt shows, I am instead having a runspace that runs a few commands every 5 minutes and then updates the associated environment variables for Oh My Posh to reference. 
+
+This works well to not slow down the loading of the Shell, but also display the information I am after. 
+
+Launch your profile configuration by typing one of the following in your PowerShell console.
+```
+# Using VSCode
+if (!(Test-Path $profile))
+{
+    New-Item -Path $profile -ItemType File -Force
+}
+code $profile
+```
+
+```
+# Not using VSCode
+if (!(Test-Path $profile))
+{
+    New-Item -Path $profile -ItemType File -Force
+}
+notepad $profile
+```
+>I am assuming there is no profile set-up yet, please take care if you are modifying an existing profile to not hurt your exsiting profile config.
+
+Copy the Microsoft.PowerShell_profile.ps1 to the profile file you just opened.
+
+
+
